ffd621e5fb54240a4bb27189c122d8fd34f55c9c
Simon Waterhouse <simon.waterhouse@citrix.com>
2013-06-06 12:51:18 +0100

    Add new configuration element for PVS device collection

---
+++ Citrix.SelfServiceDesktops.DesktopLibrary/Configuration/DeviceCollectionElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+using Citrix.SelfServiceDesktops.DesktopModel;
+
+namespace Citrix.SelfServiceDesktops.DesktopLibrary.Configuration {
+
+
+    //[XmlRootAttribute(ElementName = "device-collection")]
+   
+    public class DeviceCollectionElement : IDeviceCollection {
+
+        #region IDeviceCollection Members
+
+        [XmlAttribute("collection")]
+        public string Name  { get; set; }    
+
+        [XmlAttribute("server")]
+        public string Server  { get; set; }
+           
+
+        [XmlAttribute("site")]
+        public string Site { get; set; }
+          
+
+        #endregion
+    }
+}
---
+++ Citrix.SelfServiceDesktops.DesktopModel/IDeviceCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Citrix.SelfServiceDesktops.DesktopModel {
+
+
+    /// <summary>
+    /// Represents a PVS device collection
+    /// </summary>
+    public interface IDeviceCollection {
+
+        /// <summary>
+        /// Name of the device collection
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Name of the PVS Site
+        /// </summary>
+        string Site { get; }
+
+        /// <summary>
+        /// Name or IP address of the PVS server
+        /// </summary>
+        string Server { get; }
+    }
+}
