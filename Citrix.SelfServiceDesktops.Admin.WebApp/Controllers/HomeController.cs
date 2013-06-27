97762c365dbb316f0171a5963891a832bd3fafb2
Simon Waterhouse <simon.waterhouse@citrix.com>
2013-06-27 09:11:00 +0100

    First version of admin tool

---
+++ Citrix.SelfServiceDesktops.Admin.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Citrix.SelfServiceDesktops.DesktopModel;
+
+namespace Citrix.SelfServiceDesktops.Admin.WebApp.Controllers {
+
+    public class HomeController : Controller {
+
+      
+        public ActionResult Index() {
+
+            return View();
+        }
+
+        public ActionResult ViewDesktops() {
+            return View();
+        }
+
+        public ActionResult Edit(int item) {
+            return View();
+        }
+    }
+}
