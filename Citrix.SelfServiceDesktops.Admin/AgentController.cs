cfa147ca60a001dfe258548c9112abb9d31094fe
Simon Waterhouse <simon.waterhouse@citrix.com>
2013-07-02 13:44:11 +0100

    Add agent controller

---
+++ Citrix.SelfServiceDesktops.Admin/AgentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citrix.SelfServiceDesktops.Admin {
+
+    public class AgentController {
+
+        private string name;
+        private ServiceController controller;
+      
+
+        public AgentController(string name) {
+            this.name = name;
+            this.controller = new ServiceController(name);
+        }
+
+        /// <summary>
+        /// This method will ensure the service is stopped for the duration of the Action
+        /// method. Once the action has completed, the service is restored to its original state
+        /// </summary>
+        /// <param name="action"></param>
+        public void StopFor(Action action) {
+            ServiceControllerStatus previousStatus = WaitForStableStatus();
+            if (previousStatus != ServiceControllerStatus.Stopped) {
+                StopServiceAndWait();        
+            }
+            action();
+            RestoreService(previousStatus);
+        }
+
+        private ServiceControllerStatus WaitForStableStatus() {
+            while ((controller.Status == ServiceControllerStatus.ContinuePending) ||
+                (controller.Status == ServiceControllerStatus.PausePending) ||
+                (controller.Status == ServiceControllerStatus.StartPending) ||
+                (controller.Status == ServiceControllerStatus.StopPending)) {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                controller.Refresh();
+            }
+            return controller.Status;
+        }
+
+        private void RestoreService(ServiceControllerStatus previousStatus) {
+            if (previousStatus == ServiceControllerStatus.Running) {
+                controller.Start();
+            } else if (previousStatus == ServiceControllerStatus.Paused) {
+                controller.Pause();
+            }
+        }
+
+        private void StopServiceAndWait() {
+            controller.Stop();
+            while (controller.Status != ServiceControllerStatus.Stopped) {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                controller.Refresh();
+            }
+        }
+
+    }
+
+}
--- Citrix.SelfServiceDesktops.Admin/ConfigurationStore.cs
+++ Citrix.SelfServiceDesktops.Admin/ConfigurationStore.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.Xml.XPath;
@@ -16,9 +18,13 @@ using Citrix.SelfServiceDesktops.DesktopModel;
 
 namespace Citrix.SelfServiceDesktops.Admin
 {
+
+    /// <summary>
+    /// This class extends the base read-only class with methods to update the configuration. It operates on the configuration
+    /// file of the Agent installed on the local machine. 
+    /// </summary>
     public class ConfigurationStore : DesktopServiceConfiguration
     {
-
         private const string ServiceName = "CtxSSDSvc";
 
         public static ConfigurationStore Open() {
@@ -27,18 +33,59 @@ namespace Citrix.SelfServiceDesktops.Admin
 
         public static ConfigurationStore Open(string path) {
             if (!File.Exists(path)) {
-                string msg = string.Format("Configuration file [0} does not exist", path);
-                throw new ApplicationException(msg);
+                string msg = string.Format("Configuration file {0} does not exist", path);
+                throw new ArgumentException(msg);
+            }  
+            return new ConfigurationStore(path);
+        }
+
+        private ConfigurationStore(string path)
+            : base(path, false) {
+        }
+
+        public void Save() {
+            AgentController controller = new AgentController(ServiceName);
+            controller.StopFor(() => this.WriteFile());
+        }
+        
+        public void AddDesktopOffering(IDesktopOffering offering) {
+            XElement existing = GetDesktopOffering(offering.Name);
+            if (existing != null) {
+                throw new ArgumentException("Duplicate desktop offering name");
             }
-            XDocument doc = XDocument.Load(path);
-            return new ConfigurationStore(doc.XPathSelectElement("//selfServiceDesktops"));
+            config.XPathSelectElement("//desktopOfferings").Add(Serialize(offering));
         }
 
-        private ConfigurationStore(XElement config)
-            : base(config) {
+        public void ReplaceDesktopOffering(IDesktopOffering offering) {
+            XElement existing = GetDesktopOffering(offering.Name);
+            if (existing == null) {
+                throw new ArgumentException("Desktop offering does not exist");
+            }
+            existing.ReplaceWith(Serialize(offering));
         }
 
-       
+        public void DeleteDesktopOffering(string name) {
+            XElement existing = GetDesktopOffering(name);
+            if (existing == null) {
+                throw new ArgumentException("Desktop offering does not exist");
+            }
+            existing.Remove();
+        }
+
+        #region Private Methods
+
+        private XElement GetDesktopOffering(string name) {
+            string xpath = string.Format("//desktopOfferings/add[@name='{0}']", name);
+            return config.XPathSelectElement(xpath);
+        }
+
+        private static XElement Serialize(IDesktopOffering offering) {
+            XmlSerializer serializer = new XmlSerializer(offering.GetType());
+            MemoryStream stream = new MemoryStream();
+            serializer.Serialize(stream, offering);
+            stream.Position = 0;
+            return XElement.Load(stream);
+        }
 
         private static string GetConfigFilePath(string serviceName) {
             string keyName = string.Format(@"SYSTEM\CurrentControlSet\services\{0}", serviceName);
@@ -51,5 +98,11 @@ namespace Citrix.SelfServiceDesktops.Admin
             }
             return null;
         }
+
+        private void WriteFile() {
+            configDoc.Save(this.filePath);
+        }
+
+        #endregion
     }
 }
